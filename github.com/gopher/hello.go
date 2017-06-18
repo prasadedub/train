82f5cdf726c8e5d2b1f82890aa99e1d8f11bf28e
prasad <prasad.edub@gmail.com>
2017-06-18 16:42:46 -0400

    chap1

---
+++ github.com/gopher/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt" )
+
+func main() {
+	x:= 9
+	fmt.Printf("%v \n", x)
+	fmt.Printf("hello Gopher")
+}
---
+++ github.com/gopher/intro.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main() {
+	x:= 9
+	fmt.Printf("x is int value %v", x)
+}
